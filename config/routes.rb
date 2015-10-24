f5ce804463d3f671e9400fc1fe46918c7cee8916
Jordan Bluhm <jmbluhm@gmail.com>
2015-10-24 14:27:03 -0700

    added contacts route and model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'about/About'
 
   get 'welcome/homepage'
-
+  resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
