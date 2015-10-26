55968caecbe9cc949d18853bfcc9bc4978f8eb73
Jordan Bluhm <jmbluhm@gmail.com>
2015-10-26 14:17:02 -0700

    css fixes, parallax on experience

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -37,6 +37,22 @@ if(mq.matches){
              	 // Move the background
               	$bgobj.css(  {backgroundPosition: coords });
           	}); 
-      	});    
+			
+      	});
+		
+  	  $('section[data-type="background3"]').each(function(){
+      	  var $bgobj = $(this); // assigning the object
+  
+       	 $(window).scroll(function() {
+           	 var yPos2 = 1500 -($(window).scrollTop() / $bgobj.data('speed')); 
+          
+           	 // Put together our final background position
+           	 var coords = '50% '+ yPos2 + 'px';
+
+           	 // Move the background
+            	$bgobj.css(  {backgroundPosition: coords });
+        	}); 
+		
+    	});    
 	});
 }
