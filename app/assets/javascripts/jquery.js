71ce2ab7a4f217e22811a9582aaef50c6436907d
Jordan Bluhm <jmbluhm@gmail.com>
2015-10-26 13:36:52 -0700

    modified jquery for education scroll speed

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -24,5 +24,19 @@ if(mq.matches){
             	$bgobj.css(  {backgroundPosition: coords });
         	}); 
     	});    
+		
+    	  $('section[data-type="background2"]').each(function(){
+        	  var $bgobj = $(this); // assigning the object
+    
+         	 $(window).scroll(function() {
+             	 var yPos2 = 850 -($(window).scrollTop() / $bgobj.data('speed')); 
+            
+             	 // Put together our final background position
+             	 var coords = '50% '+ yPos2 + 'px';
+
+             	 // Move the background
+              	$bgobj.css(  {backgroundPosition: coords });
+          	}); 
+      	});    
 	});
 }
