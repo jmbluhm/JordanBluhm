1f9bf50024fc4cadcc0daeb7a680a561ae46abbc
Jordan Bluhm <jmbluhm@gmail.com>
2015-10-26 00:07:51 -0700

    added parallax to header image

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -6,3 +6,19 @@ if(k&&j[k]&&(e||j[k].data)||void 0!==d||"string"!=typeof b)return k||(k=i?a[h]=c
 $('.maps').click(function () {
     $('.maps iframe').css("pointer-events", "auto");
 });
+
+$(document).ready(function(){
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+    
+        $(window).scroll(function() {
+            var yPos = -($(window).scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
